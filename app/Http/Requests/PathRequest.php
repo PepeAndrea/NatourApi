a6d04100d7879aded13fcef8ffd3c1463dbbd356
PepeAndrea <andrea-25@outlook.it>
2022-02-02 12:07:36 +0100

    Request per inserimento percorso

---
+++ app/Http/Requests/PathRequest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\Coordinate;
+use Illuminate\Foundation\Http\FormRequest;
+
+class PathRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            //Path data
+            'title' => "required|string",
+            'description' => "required|string",
+            'location' => "required|string",
+            'difficulty_id' => "required|integer|exists:difficulties,id",
+            'disability' => "required|boolean",
+            'length' => "required|numeric|between:0.1,999.99",
+            'duration' => "required|numeric|between:0.01,99.99",
+            //Coordinates data
+            'coordinates' => 'required|array',
+            'coordinates.*.latitude' => 'required|numeric|between:-9999.9999999,9999.9999999',
+            'coordinates.*.longitude' => 'required|numeric|between:-9999.9999999,9999.9999999',
+            //Interest Point Data
+            'interest_points' => 'array',
+            'interest_points.*.title' => "required|string",
+            'interest_points.*.description' => "required|string",
+            'interest_points.*.category_id' => "required|integer|exists:categories,id",
+            'interest_points.*.latitude' => "required|numeric|between:-9999.9999999,9999.9999999",
+            'interest_points.*.longitude' => "required|numeric|between:-9999.9999999,9999.9999999",
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            //TODO: Creare messaggi di validazione
+        ];
+    }
+}
